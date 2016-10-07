377c0b59c98f466fe6357256496b4348c5a2147d
Yannick Pereira-Reis <yannick@zol.fr>
2016-10-07 09:53:27 +0200

    Disable basic auth and some tests

---
+++ tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+
+function start() {
+  make install
+}
+
+function stop() {
+  make remove
+}
+
+function test() {
+  sleep 5
+  docker ps | grep "michel-letsencrypt" | grep -q "Up" && echo "Container michel-letsencrypt is up."
+  docker ps | grep "michel" | grep -q "Up" && echo "Container michel is up."
+}
+
+function test_all() {
+  echo "=== START"
+
+  start
+  test
+  stop
+
+  echo "=== END"
+}
+
+test_all
